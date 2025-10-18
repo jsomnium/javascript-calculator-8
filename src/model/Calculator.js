e946be4c911f76236977943b0d2ce81ad2d03f71
Jisu <92006284+jsomnium@users.noreply.github.com>
2025-10-19 01:10:15 +0900

    feat: 사용자 정의 구분자 파싱 및 숫자 추출 기능 구현
    - 사용자 정의 구분자를 감지하고 추출하는 로직 추가
    - 기본 구분자와 사용자 정의 구분자를 사용하여 문자열을 분리할 수 있도록 구현
    - 구분자 배열로부터 정규 표현식을 생성하여 문자열을 분리
    - 문자열 배열을 최종적으로 정수 배열로 변환하여 반환

--- src/model/Calculator.js
+++ src/model/Calculator.js
@@ -17,15 +17,29 @@ class Calculator {
   add(inputString) {
     if (inputString === "") {
       return 0;
-    }
+      }
 
     const numbers = this.#extractNumbers(inputString);
       return this.#calculateSum(numbers);
     }
 
   #extractNumbers(inputString) {
-    const delimiters = [',', ':'];
-    let numbersString = inputString;
+      const delimiters = [',', ':'];
+      let numbersString = inputString;
+
+      // 숫자 추출을 위한 사용자 정의 구분자 처리
+      if (inputString.startsWith('//')) {
+          const delimiterEndIndex = inputString.indexOf('\n');
+          const customDelimiter = inputString.substring(2, delimiterEndIndex);
+          delimiters.push(customDelimiter);
+          numbersString = inputString.substring(delimiterEndIndex + 1);
+      }
+      
+      // 숫자 추출
+      const delimiterRegex = new RegExp(delimiters.map(d => d.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&')).join('|'));
+      const numberStrings = numbersString.split(delimiterRegex);
+      
+      return numberStrings.map(numStr => parseInt(numStr, 10));
   }
 
   #calculateSum(numbers) {
